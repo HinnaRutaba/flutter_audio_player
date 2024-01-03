beb0159ce07484791f2be179473a00345cda712b
srutaba1 <rutaba97@gmail.com>
2024-01-03 15:03:17 +0500

    added audio handler provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:audio_player/ui/home/views/home_screen.dart';
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 
 import 'config/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AudioHandler audioHandler = await AudioService.init(
+    builder: () => MyAudioHandler(),
+    config: const AudioServiceConfig(
+      androidNotificationChannelId: 'com.ryanheise.myapp.channel.audio',
+      androidNotificationChannelName: 'Audio playback',
+      androidNotificationOngoing: true,
+    ),
+  );
   runApp(const MyApp());
 }
 
